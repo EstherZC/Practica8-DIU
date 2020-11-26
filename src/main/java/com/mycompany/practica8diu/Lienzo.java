e03a916e4f9b152ac439126100a579bac54121ae
BorjaAM <borja.gallifrey@gmail.com>
2020-11-26 01:16:01 +0000

    Versión 1.2: Control de tamaño, ventanas internas

---
+++ src/main/java/com/mycompany/practica8diu/Lienzo.java
@@ -0,0 +1,29 @@
+package com.mycompany.practica8diu;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
+
+
+public class Lienzo extends JPanel{
+    
+    private BufferedImage imagen = null;
+
+    public Lienzo() {
+        this.setPreferredSize(new Dimension(1024, 768));
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.drawImage(imagen, 0, 0, null);
+    }
+    
+    public void setImagen(BufferedImage imagen){
+        this.imagen = imagen;
+        this.setPreferredSize(new Dimension(imagen.getWidth(), imagen.getHeight()));
+        this.repaint();
+    }
+    
+}
